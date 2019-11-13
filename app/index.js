c6a445e1a823dbd4c0a06624b1e9fdca85aded86
pengzhen <pengzhen@zhigui.com>
2019-11-13 14:59:24 +0800

    feat:文件上传

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')   // 解析post请求体内容
+const koaBody = require('koa-body')   // 解析post请求体内容
 const error = require('koa-json-error')  // 错误处理
 const parameter = require('koa-parameter')
+const path = require('path')
 const mongoose = require('mongoose')
+const koaStatic = require('koa-static')
+
 const { connectionStr } = require('./config')
 const routing = require('./routes')
 
@@ -13,11 +16,19 @@ mongoose.connection.on('error', console.error)
 
 const app = new Koa()
 
+app.use(koaStatic(path.join(__dirname, './public')))
+
 app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {...rest, stack}
 }))
 
-app.use(bodyparser())
+app.use(koaBody({
+  multipart: true,  // 启用文件
+  formidable: {
+    uploadDir: path.join(__dirname, './public/uploads'),
+    keepExtensions: true   // 保存拓展名
+  }
+}))
 app.use(parameter(app))
 routing(app)
 
